d93135ab04a5f3cbc14480bd77a97f70a5009d28
hehao <hehao@xiaomi.com>
2019-03-19 15:36:10 +0800

    Kernel: Xiaomi kernel changes for Redmi Note 7
    The Patch based on QualComm release TAG: LA.UM.7.2.r1-04900-sdm660.0
    The kernel config file used is lavender-perf_defconfig
    
    * pick xiaomi changes on LA.UM.7.2.r1-06500-sdm660.0
    * Conflicts:
            arch/arm/boot/dts/qcom/sdm660.dtsi
            drivers/video/fbdev/msm/mdss_dsi.h
            drivers/video/fbdev/msm/mdss_dsi_panel.c
            fs/sdcardfs/file.c
            fs/sdcardfs/inode.c
            fs/sdcardfs/lookup.c
            fs/sdcardfs/sdcardfs.h
            mm/debug.c
            net/wireless/db.txt
    
    Change-Id: I7741fc1d247e72a1cf34c051a630f59d17db3c44
    Signed-off-by: hehao <hehao@xiaomi.com>

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -77,11 +77,11 @@ static int sdcardfs_create(struct inode *dir, struct dentry *dentry,
 	}
 
 	/* save current_cred and override it */
+
 	saved_cred = override_fsids(SDCARDFS_SB(dir->i_sb),
 					SDCARDFS_I(dir)->data);
 	if (!saved_cred)
 		return -ENOMEM;
-
 	sdcardfs_get_lower_path(dentry, &lower_path);
 	lower_dentry = lower_path.dentry;
 	lower_dentry_mnt = lower_path.mnt;
@@ -143,11 +143,11 @@ static int sdcardfs_unlink(struct inode *dir, struct dentry *dentry)
 	}
 
 	/* save current_cred and override it */
+
 	saved_cred = override_fsids(SDCARDFS_SB(dir->i_sb),
 						SDCARDFS_I(dir)->data);
 	if (!saved_cred)
 		return -ENOMEM;
-
 	sdcardfs_get_lower_path(dentry, &lower_path);
 	lower_dentry = lower_path.dentry;
 	lower_mnt = lower_path.mnt;
@@ -330,7 +330,6 @@ out:
 	task_lock(current);
 	current->fs = saved_fs;
 	task_unlock(current);
-
 	free_fs_struct(copied_fs);
 out_unlock:
 	sdcardfs_put_lower_path(dentry, &lower_path);
